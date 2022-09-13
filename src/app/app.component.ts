18760bc61edcfb84bd0a51e64f8222c3751e748e
Fabian Chagua <chaguamartinkevin@gmail.com>
2022-09-13 01:43:48 -0500

    1er mensaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClientesService } from './service/clientes.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EnergyPassion';
+
+  constructor(
+    private clienteService: ClientesService )
+  {
+    this.clienteService.getClientes().subscribe(resp =>{
+      console.log(resp)
+
+    })
+  }
 }
