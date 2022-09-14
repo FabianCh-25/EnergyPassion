8e794074edd6919c9dc1529d8fbe40626155e2b2
Fabian Chagua <chaguamartinkevin@gmail.com>
2022-09-14 13:33:20 -0500

    -m "a"

---
+++ data.js
@@ -0,0 +1,53 @@
+module.exports = function () {
+  var data = {
+   clientes: [
+    {
+     id:1,
+     namePropietario: "Luis",
+     emailPropietario: "luisa@gmail.com"
+    },
+    {
+     id:2,
+     namePropietario: "Juan",
+     emailPropietario: "Juana@gmail.com",
+    },
+    {
+     id:3,
+     namePropietario: "Pedra",
+     emailPropietario: "Pedro@gmail.com",
+    },
+    {
+     id:4,
+     namePropietario: "Jesus",
+     emailPropietario: "Jesus@gmail.com",
+    }
+   ],
+/*
+
+*/
+   propietarios: [
+    {
+     id:1,
+     namePropietario: "Luisa",
+     emailPropietario: "luisa@gmail.com"
+    },
+    {
+     id:2,
+     namePropietario: "Juana",
+     emailPropietario: "Juana@gmail.com",
+    },
+    {
+     id:3,
+     namePropietario: "Pedro",
+     emailPropietario: "Pedro@gmail.com",
+    },
+    {
+     id:4,
+     namePropietario: "Jesus",
+     emailPropietario: "Jesus@gmail.com",
+    }
+   ]
+  }
+
+  return data
+ }
