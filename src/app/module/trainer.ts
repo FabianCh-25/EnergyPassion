1b2ae79a045450b4278980176e3fcbdb01abe170
Fabian Chagua <chaguamartinkevin@gmail.com>
2022-09-14 20:51:07 -0500

    se agrega clase trainers

---
+++ src/app/module/trainer.ts
@@ -0,0 +1,13 @@
+export class Trainer{
+  {
+    id:10005,
+      name: "Peter Parker",
+      age: 22,
+      nickname: "P_Parker",
+      sexo: "masculino",
+      UsuarioPremium: false,
+      email: "pparker@gmail.com",
+      horario: 1,
+      idCalificacion: 70001
+    }
+}
