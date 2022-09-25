bdef1e90a9ae2efb58b991a0d90acf773ae097b7
Fabian Chagua <chaguamartinkevin@gmail.com>
2022-09-25 17:00:54 -0500

    funciona la barra de navegar :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './page/home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PropietarioComponent } from './page/propietario/propietario.component';
@@ -5,8 +6,11 @@ import { TrainerComponent } from './page/trainer/trainer.component';
 import { ClienteComponent } from './page/cliente/cliente.component';
 import { ClienteCreaeditaComponent } from './page/cliente/cliente-creaedita/cliente-creaedita.component';
 import { ClienteDialogoComponent } from './page/cliente/cliente-listar/cliente-dialogo/cliente-dialogo.component';
-const routes: Routes = [{
+const routes: Routes = [
+  //{path: 'menu', component: HomeComponent},
+  {path: 'menu', component: HomeComponent,children:[]},
 
+  {
   path: 'clientes',component: ClienteComponent,children:[
     { path: 'nuevo', component: ClienteCreaeditaComponent },
     { path: 'edicion/:id', component: ClienteCreaeditaComponent }
