8768fede662a77b53f21f85a8f10559ca100ede0
CarlosCumpa <87941522+CarlosCumpa@users.noreply.github.com>
2022-11-09 23:34:32 -0500

    Add files via upload

--- src/app/page/rutinas/rutinas-creaedita/rutinas-creaedita.component.ts
+++ src/app/page/rutinas/rutinas-creaedita/rutinas-creaedita.component.ts
@@ -1,7 +1,12 @@
+import { Trainer } from './../../../module/trainer';
+import { RutinasService } from './../../../service/rutinas.service';
 import { RutinasService } from 'src/app/service/rutinas.service';
 import { rutinas } from './../../../module/rutinas';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Params, Router } from '@angular/router';
+import { Trainer } from 'src/app/module/trainer';
+import { TrainerService } from 'src/app/service/trainer.service';
+
 
 @Component({
   selector: 'app-rutinas-creaedita',
@@ -11,11 +16,15 @@ import { ActivatedRoute,Params, Router } from '@angular/router';
 export class RutinasCreaeditaComponent implements OnInit {
 
   rutinas:rutinas = new rutinas();
-  mensaje: string ="";
-  edicion: boolean = false;
   id: number=0;
+  edicion: boolean = false;
+  listaTrainer: Trainer[] = [];
+  idTrainerSeleccionado: number = 0;
+  mensaje: string ="";
+  
+  
 
-  constructor(private rutinasService:RutinasService ,private router: Router, private route:ActivatedRoute) { }
+  constructor(private rutinasService:RutinasService ,private router: Router, private route:ActivatedRoute, private trainerService: TrainerService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((data:Params)=>{
@@ -29,7 +38,10 @@ export class RutinasCreaeditaComponent implements OnInit {
   }
   aceptar():void{
 
-    if (this.rutinas.id > 0 && this.rutinas.Trainer_id > 0 && this.rutinas.Tren_superior.length > 0 && this.rutinas.Tren_inferior.length > 0 && this.rutinas.Tiempo > 0) {
+    if (this.rutinas.id > 0 && this.idTrainerSeleccionado > 0 && this.rutinas.Tren_superior.length > 0 && this.rutinas.Tren_inferior.length > 0 && this.rutinas.Tiempo > 0) {
+      let p = new Trainer();
+      p.id = this.idTrainerSeleccionado;
+      this.rutinas.trainer = p;
       if(this.edicion){
         this.rutinasService.modificar(this.rutinas).subscribe(data=>{
           this.rutinasService.listar().subscribe(data=>{
@@ -40,8 +52,10 @@ export class RutinasCreaeditaComponent implements OnInit {
       this.rutinasService.insertar(this.rutinas).subscribe(data => {
         this.rutinasService.listar().subscribe(data => {
           this.rutinasService.setLista(data);
-        })
-      })
+        });
+      }, err =>{
+        console.log(err);
+      });
     }
       this.router.navigate(['rutinas']);
     } else {
@@ -54,7 +68,9 @@ export class RutinasCreaeditaComponent implements OnInit {
     if(this.edicion){
       this.rutinasService.listarId(this.id).subscribe(data=> {
         this.rutinas = data;
-      })
+        console.log(data);
+        this.idTrainerSeleccionado = data.trainer.id;
+      });
     }
   }
 
