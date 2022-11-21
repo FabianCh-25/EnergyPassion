e8cde6a7dd8c3f7747ead03a2ae75c0a5b1e96a3
EMILG1808 <90169682+EMILG1808@users.noreply.github.com>
2022-11-20 23:12:28 -0500

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,19 +1,18 @@
-import { OffersInsertarComponent } from './page/offers/offers-insertar/offers-insertar.component';
-import { OffersComponent } from './page/offers/offers.component';
-import { CalificacionDialogoComponent } from './page/calificacion/calificacionlistar/calificacion-dialogo/calificacion-dialogo.component';
-import { CalificacionCreaditaComponent } from './page/calificacion/calificacion-creadita/calificacion-creadita.component';
-import { CalificacionComponent } from './page/calificacion/calificacion.component';
-import { TrainerCreaeditaComponent } from './page/trainer/trainer-creaedita/trainer-creaedita.component';
-import { RutinasCreaeditaComponent } from './page/rutinas/rutinas-creaedita/rutinas-creaedita.component';
+import { ReservacreaditaComponent } from './page/reserva/reservacreadita/reservacreadita.component';
+import { ReservaComponent } from './page/reserva/reserva.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { NgModule, Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { PropietarioComponent } from './page/propietario/propietario.component';
 import { TrainerComponent } from './page/trainer/trainer.component';
 import { ClienteComponent } from './page/cliente/cliente.component';
 import { ClienteCreaeditaComponent } from './page/cliente/cliente-creaedita/cliente-creaedita.component';
 import { ClienteDialogoComponent } from './page/cliente/cliente-listar/cliente-dialogo/cliente-dialogo.component';
-import { RutinasComponent } from './page/rutinas/rutinas.component';
+import { Calificacion } from './module/calificacion';
+import { CalificacionComponent } from './page/calificacion/calificacion.component';
+import { CalificacioncreaditaComponent } from './page/calificacion/calificacioncreadita/calificacioncreadita.component';
+import { CalificaciondeleteComponent } from './page/calificacion/calificacionlistar/calificaciondelete/calificaciondelete.component';
+
 const routes: Routes = [
   //{path: 'menu', component: HomeComponent},
   {path: 'menu', component: HomeComponent,children:[]},
@@ -26,24 +25,14 @@ const routes: Routes = [
   ]},
   {path: 'propietarios', component: PropietarioComponent,children:[]},
   {
-    path: 'trainer',component: TrainerComponent, children:[{ path: 'nuevo', component: TrainerCreaeditaComponent},
-    {path: 'edicion/:id', component: TrainerCreaeditaComponent  }
-
+  path: 'trainers',component: TrainerComponent, children:[]},
+  {path: 'calificacion', component: CalificacionComponent,children:[
+    {path: 'nuevo', component:CalificacioncreaditaComponent},
+    {path:'edicion/:id',component:CalificacioncreaditaComponent}
   ]},
-  {
-    path: 'rutinas',component: RutinasComponent, children:[
-      {path: 'nuevo', component: RutinasCreaeditaComponent},
-      {path: 'edicion/:id', component:RutinasCreaeditaComponent}
-    ]},
-    {
-    path:'calificaciones', component:CalificacionComponent,children:[
-      {path:'nuevo',component:CalificacionCreaditaComponent},
-      {path: 'edicion/:id', component:CalificacionCreaditaComponent}
-    ]},
-  { 
-    path:'offers',component:OffersComponent,children:[
-      {path:'nuevo',component:OffersInsertarComponent}
-
+    {path:'reserva',component:ReservaComponent,children:[
+      {path:'nuevo',component:ReservacreaditaComponent},
+      {path:'edicion/:id',component:ReservacreaditaComponent}
     ]}
 ];
 
